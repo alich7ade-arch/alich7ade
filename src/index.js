046dbf772256180f054e2769192e8fbd84f95b18
alich7ade-arch <alich7ade@icloud.com>
2025-11-23 12:47:18 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px", fontSize: "30px" }}>
+      <h1>Welcome Ali Ch7ade!</h1>
+      <p>Your Vercel Project Is Working Perfectly 🚀</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
